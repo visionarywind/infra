9cce9b732ebab771c18b064d7159b3e04870c7da
shanfeng <jiangshanfeng@xiaomi.com>
2018-07-11 19:45:27 +0800

    Refactor single project to multiple projects
    Structure refactor
    
    RT
    
    Signed-off-by: shanfeng <jiangshanfeng@xiaomi.com>

---
+++ app/src/main/java/com/wind/sf/App.java
@@ -0,0 +1,11 @@
+package com.wind.sf;
+
+class App {
+  public static void main(String[] args) {
+    System.out.println(greetings());
+  }
+
+  private static String greetings() {
+    return "Hi";
+  }
+}
